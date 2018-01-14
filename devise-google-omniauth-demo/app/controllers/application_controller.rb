93cb44ecded959174c377c83b9b4079b7fb19d73
theboymo <billfero@gmail.com>
2018-01-14 20:44:47 +0000

    override devise current_user method to retrun a guest user when no one is logged in

--- devise-google-omniauth-demo/app/controllers/application_controller.rb
+++ devise-google-omniauth-demo/app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   # call application concerns
   include DeviseWhitelist
 
+  #  override the devise current_user method
+  def current_user
+    # if no user is logged in return the guest user
+    super || OpenStruct.new(name: 'Guest User', first_name: 'Guest', last_name: 'User', email: 'guest@example.com')
+  end
 end

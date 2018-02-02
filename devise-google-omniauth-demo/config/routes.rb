1c93a336466194c7fdbd582973fca9ba07992e8f
theboymo <billfero@gmail.com>
2018-02-02 12:42:15 +0000

    review app devise routes

--- devise-google-omniauth-demo/config/routes.rb
+++ devise-google-omniauth-demo/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   resources :blogs
 	# customize sign_in, _up, _out routes
+	# creates the paths
+	# /auth/google/callback
+	# /auth/github/callback
 	devise_for :users, path: '', path_names: {
 			sign_in: 'login',   # 'users/sign_in' => 'login'
 			sign_out: 'logout', # 'users/sign_out' => 'logout'
@@ -9,6 +12,14 @@ Rails.application.routes.draw do
 	}, controllers: {
 			omniauth_callbacks: 'users/omniauth_callbacks'
 	}
+
+
+	# create the following paths
+	# /users/auth/google/callback
+	# /users/auth/github/callback
+	# devise_for :users, controllers: {
+	# 		omniauth_callbacks: 'users/omniauth_callbacks'
+	# }
 
 	root to: "blogs#index"
 end

9f0e35dd8142deb968c402db9e1fcc809d6c90e3
theboymo <billfero@gmail.com>
2018-02-03 12:18:20 +0000

    update Gemfile with additional test gems

---
+++ cuke-auth-demo/spec/controllers/visitors_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe VisitorsController do
+	let(:valid_params){ {name: 'Ivan', email: 'my@email.com', message: 'Love your site!'} }
+	it 'renders index template' do
+		get :index
+		expect(response).to render_template('index')
+	end
+
+	it 'assigns event to next_occurrence' do
+		event = double(Event)
+		expect(Event).to receive(:next_occurrence).with(:Scrum).and_return(event)
+		get :index
+		expect(assigns(:event)).to eq event
+	end
+end

6389e15643e979351705ffa690d76c82d252cafe
theboymo <billfero@gmail.com>
2018-02-20 13:49:32 +0000

    rails devise setup with email confirmation

---
+++ devise-email-facebook-roles-demo/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+	devise_for :users, path: '', path_names: {
+			sign_in: 'login',   # 'users/sign_in' => 'login'
+			sign_out: 'logout', # 'users/sign_out' => 'logout'
+			sign_up: 'register' # 'users/sign_up' => 'register'
+	},
+	controllers: { registrations: 'registrations' }
+
+	root to: 'static#home'
+
+	get 'about', to: 'static#about'
+end

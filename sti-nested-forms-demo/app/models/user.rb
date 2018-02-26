e330a103a9b285cd8a11a0a0fe90279add3bbf9d
theboymo <billfero@gmail.com>
2018-02-26 10:11:39 +0000

    add users controller

--- sti-nested-forms-demo/app/models/user.rb
+++ sti-nested-forms-demo/app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
 
-	# models which subclass user0
+	# models which subclass user
 	def self.types
 		%w(Developer Founder)
 	end
+
+	# define scope
+	scope :developers, -> {where(type: 'Developer')}
+	scope :founders, -> {where(type: 'Founder')}
 end

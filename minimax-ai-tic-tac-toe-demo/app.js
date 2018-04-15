d4ea7d4ed449c518c79d32c39a8b7bfee1bbea38
theboymo <billfero@gmail.com>
2018-04-15 15:58:37 +0100

    add basic game state and click listener to squares

--- minimax-ai-tic-tac-toe-demo/app.js
+++ minimax-ai-tic-tac-toe-demo/app.js
@@ -1,1 +1,26 @@
 //  app javascript
+
+let state;
+const player = 'O'
+const computer = 'X'
+const winCombinations = [
+  [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+]
+const cells = document.querySelectorAll('.cell');
+const message = document.querySelector('#message')
+
+startGame();
+
+function startGame(){
+  document.querySelector('#endgame').style.display = 'none';
+  state = Array.from(Array(9).keys());
+  for(let i = 0; i < cells.length; i++){
+    cells[i].innerText = '';
+    cells[i].style.removeProperty('background-color');
+    cells[i].addEventListener('click', turnClick, false);
+  }
+}
+
+function turnClick(e){
+  console.log(e.target.id);
+}

c844c36319eb13f8eaeb0d787e34bbd6a6efa4f9
theboymo <billfero@gmail.com>
2018-04-15 17:50:17 +0100

    implment check for win and game over logic

--- minimax-ai-tic-tac-toe-demo/app.js
+++ minimax-ai-tic-tac-toe-demo/app.js
@@ -1,7 +1,7 @@
 //  app javascript
 
-let board;
-const player = 'O'
+let origBoard;
+const human = 'O'
 const computer = 'X'
 const winCombinations = [
   [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
@@ -13,7 +13,7 @@ startGame();
 
 function startGame(){
   document.querySelector('#endgame').style.display = 'none';
-  board = Array.from(Array(9).keys());
+  origBoard = Array.from(Array(9).keys());
   for(let i = 0; i < cells.length; i++){
     cells[i].innerText = '';
     cells[i].style.removeProperty('background-color');
@@ -23,11 +23,41 @@ function startGame(){
 
 // human move
 function turnClick(e){
-  turn(e.target.id, player)
+  turn(e.target.id, human)
 }
 
 // update the boards state and display the move
 function turn(squareId, player){
-  board[squareId] = player;
+  origBoard[squareId] = player;
   document.getElementById(squareId).innerText = player;
+  let gameWon = checkWin(origBoard, player);
+  if (gameWon) gameOver(gameWon)
+}
+
+function checkWin(board, player){
+  // iterate through the board array to see if the player has a winning combination
+  let plays = board.reduce(function(combo, elm, index){
+    return (elm === player)? combo.concat(index) : combo;
+  }, [])
+
+  let gameWon = null;
+  for(let[index, win] of winCombinations.entries()){
+    if (win.every(elem => plays.indexOf(elem) > -1)){
+      gameWon = {index: index, player: player};
+      break;
+    }
+  }
+  return gameWon;
+}
+
+
+function gameOver(gameWon){
+  for(let index of winCombinations[gameWon.index]){
+    document.getElementById(index).style.backgroundColor = 
+      (gameWon.player == human)? 'blue' : 'red';
+  }
+  for(let i = 0; i < cells.length; i++){
+    // remove the event listener so no more cells can be clicked
+    cells[i].removeEventListener('click', turnClick, false);
+  }
 }

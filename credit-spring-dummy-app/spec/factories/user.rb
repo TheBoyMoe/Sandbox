afc60fb90b62e0492f111f84b9546de1e7405a04
theboymo <billfero@gmail.com>
2018-04-19 09:36:57 +0100

    refactored model specs to use FactoryBot

--- credit-spring-dummy-app/spec/factories/user.rb
+++ credit-spring-dummy-app/spec/factories/user.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user, class: User do
     email 'joe@joe.com'
+    password '1234'
   end
 end
--- credit-spring-dummy-app/spec/models/user_spec.rb
+++ credit-spring-dummy-app/spec/models/user_spec.rb
@@ -2,23 +2,28 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'is invalid when the email address is not in the whitelist' do
-    user = User.new(email: 'z@z.com', password: '1234')
+    # user = User.new(email: 'z@z.com', password: '1234')
+    user = FactoryBot.build(:user, email: 'z@z.com')
     expect(user).to be_invalid
   end
 
   it 'is valid when the email address is in the whitelist' do
-    user = User.new(email: 'a@a.com', password: '1234')
+    # user = User.new(email: 'a@a.com', password: '1234')
+    user = FactoryBot.build(:user, email: 'a@a.com')
     expect(user).to be_valid
   end
 
   it 'is invalid when the email address is not provided' do
-    user = User.new(email: '', password: '1234')
+    # user = User.new(email: '', password: '1234')
+    user = FactoryBot.build(:user, email: '')
     expect(user).to be_invalid
   end
 
   it 'is invalid if the email addres is not unique' do
-    User.create(email: 'a@a.com', password: '1234')
-    user = User.new(email: 'a@a.com', password: '1234')
+    # User.create(email: 'a@a.com', password: '1234')
+    # user = User.new(email: 'a@a.com', password: '1234')
+    FactoryBot.create(:user, email: 'a@a.com')
+    user = FactoryBot.build(:user, email: 'a@a.com')
     expect(user).to be_invalid
   end
 end

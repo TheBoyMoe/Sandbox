908c36c144395589b1bdeb1fbf3aaa52cfd3c916
theboymo <billfero@gmail.com>
2018-04-19 14:21:28 +0100

    implement database table to maintain white list

---
+++ credit-spring-dummy-app/app/models/whitelist.rb
@@ -0,0 +1,1 @@
+class Whitelist < ApplicationRecord; end
--- credit-spring-dummy-app/spec/models/user_spec.rb
+++ credit-spring-dummy-app/spec/models/user_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  it 'is invalid if user is not in whitelist table' do
+    whitelist_user = Whitelist.create(email: 'a@a.com')
+    user = FactoryBot.build(:user, email: whitelist_user.email)
+    expect(user).to be_valid
+  end
+
   it 'is invalid when the email address is not in the whitelist' do
-    # user = User.new(email: 'z@z.com', password: '1234')
+    whitelist_user = Whitelist.create(email: 'a@a.com')
     user = FactoryBot.build(:user, email: 'z@z.com')
     expect(user).to be_invalid
   end
 
-  it 'is valid when the email address is in the whitelist' do
-    # user = User.new(email: 'a@a.com', password: '1234')
-    user = FactoryBot.build(:user, email: 'a@a.com')
-    expect(user).to be_valid
-  end
 
   it 'is invalid when the email address is not provided' do
     # user = User.new(email: '', password: '1234')

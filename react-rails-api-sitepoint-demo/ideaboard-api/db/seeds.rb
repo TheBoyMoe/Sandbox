ae600c83ea6e590518da13fbda991e8a31ede23a
theboymo <billfero@gmail.com>
2018-06-11 16:14:19 +0100

    basic rails api working and publishing idea objs in json format

--- react-rails-api-sitepoint-demo/ideaboard-api/db/seeds.rb
+++ react-rails-api-sitepoint-demo/ideaboard-api/db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ideas = Idea.create(
+  [
+    {
+      title: '01 A new cake recipe',
+      body: 'more dummy content'
+    },
+    {
+      title: '02 A new cake recipe',
+      body: 'more dummy content'
+    },
+    {
+      title: '03 A new cake recipe',
+      body: 'more dummy content'
+    },
+    {
+      title: '04 A new cake recipe',
+      body: 'more dummy content'
+    },
+    {
+      title: '05 A new cake recipe',
+      body: 'more dummy content'
+    }
+  ]
+)

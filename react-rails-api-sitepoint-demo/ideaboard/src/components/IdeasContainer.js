3438d7e7f172915951ca9019038a1e46a283060e
theboymo <billfero@gmail.com>
2018-06-11 18:14:30 +0100

    create new blank ideas and update the apps state

--- react-rails-api-sitepoint-demo/ideaboard/src/components/IdeasContainer.js
+++ react-rails-api-sitepoint-demo/ideaboard/src/components/IdeasContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import update from 'immutability-helper';
 
 import Idea from './Idea';
 
@@ -19,9 +20,26 @@ export default class IdeasContainer extends React.Component {
       .catch(err => console.log(err));
   }
 
+  // submit new idea to rails api
+  addNewIdea = () => {
+    axios.post('http://localhost:3001/api/v1/ideas', {
+      idea: { title: '', body: ''}
+      })
+      .then(res => {
+        console.log(res);
+        // update the state immutably
+        const updatedIdeas = update(this.state.ideas, {$splice: [[0, 0, res.data]]});
+        this.setState({ ideas: updatedIdeas });
+      })
+      .catch(err => console.log(err));
+  }
+
   render(){
     return(
       <div>
+        <button 
+          onClick={ this.addNewIdea }
+          className="newIdeaButton">Add an Idea</button>
         <ul>
           { this.state.ideas.map(idea => {
             return <Idea key={idea.id} { ...idea }/>

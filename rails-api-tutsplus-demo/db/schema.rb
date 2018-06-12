64c3a08073e07490b5ee679ecbda1083967e5971
theboymo <billfero@gmail.com>
2018-06-12 14:57:39 +0100

    issue each user a unique token whan they are created

--- rails-api-tutsplus-demo/db/schema.rb
+++ rails-api-tutsplus-demo/db/schema.rb
@@ -1,4 +1,16 @@
-ActiveRecord::Schema.define(version: 20180612121724) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20180612133850) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -13,6 +25,8 @@ ActiveRecord::Schema.define(version: 20180612121724) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
+    t.index ["token"], name: "index_users_on_token"
   end
 
 end

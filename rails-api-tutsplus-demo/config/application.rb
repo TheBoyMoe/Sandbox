3dfbf3984ef823910f9c6202cc9921b1960f54d8
theboymo <billfero@gmail.com>
2018-06-12 17:52:49 +0100

    setup and enable rate limiting using rack attack

--- rails-api-tutsplus-demo/config/application.rb
+++ rails-api-tutsplus-demo/config/application.rb
@@ -29,5 +29,8 @@ module RailsApiTutsplusDemo
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # enable RackAttack
+    config.middleware.use Rack::Attack
   end
 end

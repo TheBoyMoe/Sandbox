c68ed644acb95dea96afe659618dcd0d0bd16027
theboymo <billfero@gmail.com>
2018-06-12 13:28:12 +0100

    initial commit - tutsplus rails 5 api demo - basic install with user and posts models

---
+++ rails-api-tutsplus-demo/db/schema.rb
@@ -0,0 +1,18 @@
+ActiveRecord::Schema.define(version: 20180612121724) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end

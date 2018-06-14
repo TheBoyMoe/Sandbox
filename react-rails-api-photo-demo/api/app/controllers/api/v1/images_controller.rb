0054bb4a646eb06b0595e067a22e0686ad5aed75
theboymo <billfero@gmail.com>
2018-06-14 18:27:42 +0100

    implement images controller, show, index, create and delete via api

--- react-rails-api-photo-demo/api/app/controllers/api/v1/images_controller.rb
+++ react-rails-api-photo-demo/api/app/controllers/api/v1/images_controller.rb
@@ -1,9 +1,44 @@
 module Api
   module V1
     class ImagesController < ApplicationController
+      before_action :set_user, only: %i(create destroy)
+
       def index
         @images = Image.order('created_at DESC')
         render json: @images
+      end
+
+      def create
+        @image = @user.images.new(image_params)
+        if @image.save
+          render json: @image, status: :created
+        else
+          render json: @image.errors, status: :unprocessable_entity
+        end
+      end
+
+      def show
+        @image = Image.find(params[:id])
+        render json: @image
+      end
+
+      def destroy
+        @image = @user.images.find(params[:id])
+        if @image
+          @image.destroy
+        else
+          render json: { image: 'not found'}, status: :not_found
+        end
+      end
+
+      private
+
+      def image_params
+        params.require(:image).permit(:title, :path)
+      end
+
+      def set_user
+        @user = User.find(1)
       end
     end
   end
--- react-rails-api-photo-demo/api/config/routes.rb
+++ react-rails-api-photo-demo/api/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :images, only: [:index, :create, :show]
-      resources :users, only: [:create, :show]
+      resources :images, only: %i(index create show destroy)
+      resources :users, only: %i(create show)
     end
   end
 end

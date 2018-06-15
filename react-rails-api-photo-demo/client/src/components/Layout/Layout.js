4d3f1ee85af12d8dae5d859f363a40895bc59560
theboymo <billfero@gmail.com>
2018-06-15 14:21:26 +0100

    implement basic layout component

---
+++ react-rails-api-photo-demo/client/src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
+import './Layout.css';
+
+const layout = (props) => {
+  return (
+    <div>
+      <Toolbar />
+      <main className="Content">
+        { props.children }
+      </main>
+    </div>
+  );
+};
+
+export default layout;

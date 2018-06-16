9de9cfc23ea19c071da7b5854dd087f6b385984c
theboymo <billfero@gmail.com>
2018-06-16 10:59:48 +0100

    Implement react-bootstrp libray and add the logo to collapsing toolbar

--- react-rails-api-photo-demo/client/src/components/Toolbar/Toolbar.js
+++ react-rails-api-photo-demo/client/src/components/Toolbar/Toolbar.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import NavItems from './NavItems/NavItems';
+// import NavItems from './NavItems/NavItems';
 import Logo from './Logo/Logo';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 import './Toolbar.css';
 
 const toolbar = () => {
   return(
-    <header className="Toolbar">
-      <Logo />
-      <NavItems />     
-    </header>
+    // <header className="Toolbar">
+    //   <Logo />
+    //   <NavItems />     
+    // </header>
+    <Navbar collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          {/* <a href="#brand">React-Bootstrap</a> */}
+          <Logo />
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#">
+            Link Right
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            Link Right
+          </NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 export default toolbar;

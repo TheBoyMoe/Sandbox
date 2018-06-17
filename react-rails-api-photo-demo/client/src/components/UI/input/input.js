dd08675d455db4ab1606e00852d5a8d45cf36feb
theboymo <billfero@gmail.com>
2018-06-17 09:54:32 +0100

    update input component to not display label element for submit type

--- react-rails-api-photo-demo/client/src/components/UI/input/input.js
+++ react-rails-api-photo-demo/client/src/components/UI/input/input.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './input.css';
 
 const input = (props) => {
+  let label = null;
+  if(props.label)
+    label = <label>{ props.label }</label> ;
+
   return (
     <div className="Input">
-      <label>{ props.label }</label>
+      { label } 
       <input { ...props }/>
     </div>
   );

efab1fa12c57fee652f0f8b4dcada1563f0635ee
theboymo <billfero@gmail.com>
2018-06-20 09:11:42 +0100

    create a redux store and connect it to the app

--- react-rails-api-photo-demo/demo-app-with-redux/client/src/index.js
+++ react-rails-api-photo-demo/demo-app-with-redux/client/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
+import reducer from './store/reducer';
 import registerServiceWorker from './registerServiceWorker';
 
+const appStore = createStore(reducer);
+
 const app = (
-  <Router>
-    <App />
-  </Router>
+  <Provider store={ appStore }>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
---
+++ react-rails-api-photo-demo/demo-app-with-redux/client/src/store/reducer.js
@@ -0,0 +1,13 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+  mobileMenuOpen: false,
+  token: null
+};
+
+const reducer = (state = initialState, action) => {
+
+};
+
+
+export default reducer;

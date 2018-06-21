46931614e78eec4e9c67b166f365314a16b79136
theboymo <billfero@gmail.com>
2018-06-21 09:28:55 +0100

    implement the login reducer

--- react-rails-api-photo-demo/demo-app-with-redux/client/src/index.js
+++ react-rails-api-photo-demo/demo-app-with-redux/client/src/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
-import reducer from './store/reducers/auth';
+import authReducer from './store/reducers/auth';
 import registerServiceWorker from './registerServiceWorker';
+
+const rootReducer = combineReducers({
+  auth: authReducer
+});
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ?
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const appStore = createStore(
-  reducer, composeEnhancers(applyMiddleware(thunk))
+  rootReducer, composeEnhancers(applyMiddleware(thunk))
 );
 
 const app = (
--- react-rails-api-photo-demo/demo-app-with-redux/client/src/store/reducers/auth.js
+++ react-rails-api-photo-demo/demo-app-with-redux/client/src/store/reducers/auth.js
@@ -1,11 +1,33 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-  token: null
+  token: null,
+  error: null,
+  redirectPath: '/explore'
 };
 
 const reducer = (state = initialState, action) => {
-
+  switch(action.type){
+  case actionTypes.LOGIN_USER:
+    return {
+      ...state,
+      error: null
+    };
+  case actionTypes.LOGIN_SUCCESS:
+    return {
+      ...state,
+      token: action.token,
+      error: null
+    };
+  case actionTypes.LOGIN_FAILURE:
+    return {
+      ...state,
+      token: null,
+      error: state.error
+    };
+  default:
+    return state;
+  }
 };
 
 

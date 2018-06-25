6bee831a121e6ab982a678639e43b18cf2e17ffb
theboymo <billfero@gmail.com>
2018-06-25 14:13:55 +0100

    render a list of gallery titles and descriptions

--- react-rails-api-photo-demo/demo-app-with-redux-and-gallery/client/src/containers/Gallery/Index/Index.js
+++ react-rails-api-photo-demo/demo-app-with-redux-and-gallery/client/src/containers/Gallery/Index/Index.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import axiosClient from '../../../axiosClient';
 
 import './Index.css';
 
 class GalleryIndex extends React.Component {
+  state = {
+    galleries: []
+  }
+
+  componentDidMount(){
+    axiosClient.get('/galleries')
+      .then(res => {
+        console.log(res.data); // response depends on axios
+        this.setState({ galleries: res.data });
+      });    
+  }
+
+  renderGalleryList = () => {
+    const galleries = this.state.galleries;
+    return (
+      galleries.map(gallery => {
+        return <li key={ gallery.id }>
+          <h3>{ gallery.title }</h3>
+          <p>{ gallery.description }</p>
+        </li>
+      })
+    )
+  }
+
   render(){
     return(
       <div className="GalleryIndex">
         <h1>Gallery</h1>
+        <ul>
+          { this.renderGalleryList() }
+        </ul>
       </div>  
     );
   }

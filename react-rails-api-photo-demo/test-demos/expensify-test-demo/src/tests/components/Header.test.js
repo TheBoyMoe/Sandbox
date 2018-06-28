e9b71b6a6478263493f71e3e7970f3bd6dee624b
theboymo <billfero@gmail.com>
2018-06-28 09:05:25 +0100

    implement shallow test on Header component

--- react-rails-api-photo-demo/test-demos/expensify-test-demo/src/tests/components/Header.test.js
+++ react-rails-api-photo-demo/test-demos/expensify-test-demo/src/tests/components/Header.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+// import toJSON from 'enzyme-to-json';
+// import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
 
 test('should render Header component', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  // console.log(renderer.getRenderOutput()); // DEBUG
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  // expect(wrapper.find('h1').length).toBe(1);
+  // expect(wrapper.find('h1').text()).toBe('Expensify');
+  // expect(toJSON(wrapper)).toMatchSnapshot(); // loaded via jest.config.js file
+  expect(wrapper).toMatchSnapshot();
+
+  // shallow render using react renderer
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });

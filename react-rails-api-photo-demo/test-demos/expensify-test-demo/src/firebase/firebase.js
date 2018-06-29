712594dd3ef3418e640aa616bdeea8f7b6c2d0a3
theboymo <billfero@gmail.com>
2018-06-29 19:22:27 +0100

    add firebase configuration

---
+++ react-rails-api-photo-demo/test-demos/expensify-test-demo/src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyCp_-a33zFQDI-yYW7VG6CNSLUE4aHSCc0',
+  authDomain: 'expensify-demo-50d60.firebaseapp.com',
+  databaseURL: 'https://expensify-demo-50d60.firebaseio.com',
+  projectId: 'expensify-demo-50d60',
+  storageBucket: 'expensify-demo-50d60.appspot.com',
+  messagingSenderId: '302657365617'
+};
+
+// firebase's set() method returns a promise
+firebase.initializeApp(config);
+firebase.database().ref().set({
+  name: 'Expensify App'
+})
+  .then(res => console.log('result:', res)) // returns undefined
+  .catch(err => console.log('Firebase Error:', err)); 

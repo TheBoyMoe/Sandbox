f64ce9febb771058547a29075d6229909b069b29
theboymo <billfero@gmail.com>
2018-06-30 18:46:25 +0100

    config tests to write to a test database

--- react-rails-api-photo-demo/test-demos/expensify-test-demo/src/firebase/firebase.js
+++ react-rails-api-photo-demo/test-demos/expensify-test-demo/src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyCp_-a33zFQDI-yYW7VG6CNSLUE4aHSCc0',
-  authDomain: 'expensify-demo-50d60.firebaseapp.com',
-  databaseURL: 'https://expensify-demo-50d60.firebaseio.com',
-  projectId: 'expensify-demo-50d60',
-  storageBucket: 'expensify-demo-50d60.appspot.com',
-  messagingSenderId: '302657365617'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 // firebase's set() method returns a promise
@@ -14,7 +14,7 @@ firebase.initializeApp(config);
 const database = firebase.database();
 
 export { firebase, database as default };
-
+ 
 // firebase.database().ref().set({
 //   name: 'Expensify App'
 // })
